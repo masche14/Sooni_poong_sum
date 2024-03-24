03bfb5c377ab79637421dbbccee8212ee4f016c2
masche14 <162661048+masche14@users.noreply.github.com>
2024-03-24 20:57:14 +0900

    Add files via upload

--- 2024.03.24_sooni_sql.sql
+++ 2024.03.24_sooni_sql.sql
@@ -1,25 +1,19 @@
 use sooni_group;
 
-# 신규방식 테스트 - 2024.03.18
+# 비제이 id 컬럼 유니크 지정
+alter table bj modify column id varchar(20) unique;
+
+# 비제이 명단 조회
+select * from bj;
+
+# daily_income 테이블 date 컬럼 날짜 형식 지정
+alter table daily_income modify column date date;
 
 # 일일 수익 데이터 조회
 select *
 from daily_income;
 
-# 총 데이터 개수 조회
-select count(*)
-from daily_income;
-
-# 특정 기간 내 비제이별  수익 데이터 총합 도출
-select id, sum(poong) as poong_sum
-from daily_income
-where date between '2024-02-08' and '2024-03-07'
-group by id;
-
 # bj 테이블과 join 하여 내용 확인
-
-select date(now());
-
 select b.*, d.poong_sum as f_income
 from bj b
 left join (select id, sum(poong) as poong_sum
@@ -30,8 +24,7 @@ on b.id=d.id
 order by department, f_income desc;
 
 select *
-from daily_income
-where date = '2024-03-19';
+from daily_income;
 
 # 이번달 수익 데이터, 지난 달 수익 데이터 차이 가상 테이블 생성
 with poong_insight as (
